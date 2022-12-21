8a128ea5bc91e3b18ea25f0bbd94eaad0c01c54a
maxwellmuchiri <muchirimaxwel@gmail.com>
2022-12-21 22:35:41 +0300

    continuation

--- JS/index.js
+++ JS/index.js
@@ -1,45 +1,27 @@
 //FETCHING DATA FROM JSON SERVER
 let url =" http://localhost:3000/vehicles"
-
-// function fetchVehicles(url) {
-//     fetch(url)
-//         .then(response => response.json())
-//         .then(vehicles => {
-//             vehicles.show.forEach(vehicle=> {
-//                 displayVehicle(vehicle)
-//             });
-//         })
-
-// }
-// function displayVehicle(vehicle)
-// {
-
-//     const li = document.createElement('li')
-//     li.style.cursor = "pointer"
-//     li.textContent = (vehicle.type).toUpperCase()
-//     listHolder.appendChild(li)
-    
-    
-// }
+ 
 const mboto = document.getElementById("mboto").addEventListener('click',(x)=>{console.log("wtfff")})
 const price = document.getElementById("price").addEventListener('click',(x)=>{console.log("wtfff")})
 const type = document.getElementById("make").addEventListener('click',(x)=>{console.log("wtfff")})
+const vehicles=document.querySelector(".vehicles");
 
 function fetchVehicles(){
     fetch(url)
     .then(resp=>resp.json()
     )
-    .then(data=>{ if(data.type ="Car"){
-        console.log(data.id)
-    }});
+    .then(data=>{
 
-    // if(data.type ="Car"){
-    //     console.log(data.id)
-    // }
+    const li = document.createElement('ul');
+    data.forEach(vehicle => {
+      const item = document.createElement('li');
+      item.textContent = `${vehicle.make} ${vehicle.model} (img src"${vehicle.link}")`;
+      li.appendChild(item);
+    });
+    // add the list to the page
+    document.body.appendChild(li);
+  });
 
+    
 }
 fetchVehicles()
-
-// fetch('https://example.com/data.json')
-//   .then(response => response.json())
-//   .then(data => console.log(data));
