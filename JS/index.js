5c538d8ad6632f5ab82b9d136cf3c6c51510ab41
maxwellmuchiri <muchirimaxwel@gmail.com>
2023-01-06 02:15:31 +0300

    adding service rating

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,32 @@
 // FETCHING DATA FROM JSON SERVER
 let url ="http://localhost:3000/vehicles";
-const mboto = document.getElementById("mboto").addEventListener('click',(x)=>{console.log("yess")})
-const price = document.getElementById("price").addEventListener('click',(x)=>{console.log("yess")})
-const type = document.getElementById("make").addEventListener('click',(x)=>{console.log("wtfff")})
-const vehicles=document.querySelector(".vehicles");
-const imageElement = document.createElement('img');
+let mboto = document.getElementById("mboto").addEventListener('click',(x)=>{console.log("yess")})
+let price = document.getElementById("price").addEventListener('click',(x)=>{console.log("yess")})
+let type = document.getElementById("make").addEventListener('click',(x)=>{console.log("wtfff")})
+let vehicles=document.querySelector(".vehicles");
+let imageElement = document.createElement('img');
+let mainpage=document.getElementsByClassName('mainpage');
 
 function fetchVehicles(){
     fetch(url)
     .then(resp=>resp.json()
     )
     .then(data=>{
-
-    const li = document.createElement('ul');
+    let li = document.createElement('ul');
     data.forEach(vehicle => {
       const item = document.createElement('li');
-      item.textContent = `${vehicle.make} ${vehicle.price} ${vehicle.photo}`;
+      item.textContent = `${vehicle.make} ${vehicle.price} <img src="${vehicle.photo}"`;
       li.appendChild(item);
-      imageElement.src = data.imageUrl;
-      document.body.appendChild(imageElement);
     });
     document.body.appendChild(li);
   });
 }
 fetchVehicles()
+
+let form = document.getElementById('ratingForm');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let rating = form.elements.rating.value;
+  console.log(`You rated your service as ${rating} out of 5.`);
+});
+
